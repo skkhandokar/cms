a461d089b762f02e108ab35304dcd7dd096e2b28
Khandokar Samadul Kabir <111075442+skkhandokar@users.noreply.github.com>
2025-03-26 17:26:55 +0600

    Update page.js

--- frontend/src/app/Staffs/page.js
+++ frontend/src/app/Staffs/page.js
@@ -10,21 +10,21 @@ export default function StaffHome() {
   const [staff, setStaff] = useState([]);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/team/chairmen/").then((res) => setChairmen(res.data));
-    axios.get("http://127.0.0.1:8000/api/team/managing-directors/").then((res) => setDirectors(res.data));
-    axios.get("http://127.0.0.1:8000/api/team/staff/").then((res) => setStaff(res.data));
+    axios.get("https://skkhandokar18.pythonanywhere.com/api/team/chairmen/").then((res) => setChairmen(res.data));
+    axios.get("https://skkhandokar18.pythonanywhere.com/api/team/managing-directors/").then((res) => setDirectors(res.data));
+    axios.get("https://skkhandokar18.pythonanywhere.com/api/team/staff/").then((res) => setStaff(res.data));
   }, []);
 
   return (
     <div className="bg-gray-100 min-h-screen">
 
-      {/* Welcome Section */}
+    
       <div className="text-center py-16">
         <h1 className="text-4xl font-bold text-blue-800">Welcome to Our Diagnostic Center</h1>
         <p className="text-lg text-gray-600 mt-4">Meet our team and explore our services.</p>
       </div>
 
-      {/* Chairman Section */}
+
       <div className="py-16 bg-gradient-to-r from-blue-50 to-blue-100">
         <h2 className="text-4xl font-bold text-blue-800 text-center mb-12">Our Chairmen</h2>
         <div className="gap-8 grid-cols-1 max-w-sm mx-auto">
@@ -34,7 +34,7 @@ export default function StaffHome() {
         </div>
       </div>
 
-      {/* Managing Director Section */}
+   
       <div className="py-16 bg-gradient-to-r from-blue-100 to-blue-200">
         <h2 className="text-4xl font-bold text-blue-800 text-center mb-12">Managing Directors</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
@@ -44,7 +44,7 @@ export default function StaffHome() {
         </div>
       </div>
 
-      {/* Staff Section */}
+
       <div className="py-16 bg-gradient-to-r from-blue-50 to-blue-100">
         <h2 className="text-4xl font-bold text-blue-800 text-center mb-12">Our Staff</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
